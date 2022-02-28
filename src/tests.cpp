c5eb7ecd630f336a996b9710ae12abfacc683d98
Edward@papaya <edward@papaya>
2022-02-27 16:45:08 -0800

    tests

--- src/tests.cpp
+++ src/tests.cpp
@@ -2,10 +2,75 @@
 #include "gtest/gtest.h"
 
 TEST(GraphTest, Init) {
-  Graph<3, 5, 10> g;
-  g.add_edge(0b11100, UNDIRECTED);
-  g.add_edge(0b1100010, 5);
-  g.add_edge(0b1110, 2);
-  g.add_edge(0b1011, UNDIRECTED);
+  Graph<3, 7, 10> g;
+  g.add_edge(0b11100, UNDIRECTED);  // 234
+  g.add_edge(0b1100010, 5);         // 156>5
+  g.add_edge(0b1110, 2);            // 123>2
+  g.add_edge(0b1011, UNDIRECTED);   // 013
   g.init();
+
+  EXPECT_EQ(4, g.edge_count);
+  EXPECT_EQ(g.edges[0].vertex_set, 0b1011);
+  EXPECT_EQ(g.edges[0].head_vertex, UNDIRECTED);
+  EXPECT_EQ(g.edges[1].vertex_set, 0b1110);
+  EXPECT_EQ(g.edges[1].head_vertex, 2);
+  EXPECT_EQ(g.edges[2].vertex_set, 0b11100);
+  EXPECT_EQ(g.edges[2].head_vertex, UNDIRECTED);
+  EXPECT_EQ(g.edges[3].vertex_set, 0b1100010);
+  EXPECT_EQ(g.edges[3].head_vertex, 5);
+
+  EXPECT_EQ(g.vertices[0].degree_undirected, 1);
+  EXPECT_EQ(g.vertices[0].degree_head, 0);
+  EXPECT_EQ(g.vertices[0].degree_tail, 0);
+
+  EXPECT_EQ(g.vertices[1].degree_undirected, 1);
+  EXPECT_EQ(g.vertices[1].degree_head, 0);
+  EXPECT_EQ(g.vertices[1].degree_tail, 2);
+
+  EXPECT_EQ(g.vertices[2].degree_undirected, 1);
+  EXPECT_EQ(g.vertices[2].degree_head, 1);
+  EXPECT_EQ(g.vertices[2].degree_tail, 0);
+
+  EXPECT_EQ(g.vertices[3].degree_undirected, 2);
+  EXPECT_EQ(g.vertices[3].degree_head, 0);
+  EXPECT_EQ(g.vertices[3].degree_tail, 1);
+}
+
+TEST(GraphTest, T3) {
+  Graph<3, 5, 5> g;
+  g.add_edge(0b1011, UNDIRECTED);  // 013
+  g.add_edge(0b1110, 2);           // 123>2
+  g.add_edge(0b1101, UNDIRECTED);  // 023
+  g.add_edge(0b11100, 2);          // 234>2
+  g.init();
+
+  EXPECT_EQ(4, g.edge_count);
+  EXPECT_EQ(g.edges[0].vertex_set, 0b1011);
+  EXPECT_EQ(g.edges[0].head_vertex, UNDIRECTED);
+  EXPECT_EQ(g.edges[1].vertex_set, 0b1101);
+  EXPECT_EQ(g.edges[1].head_vertex, UNDIRECTED);
+  EXPECT_EQ(g.edges[2].vertex_set, 0b1110);
+  EXPECT_EQ(g.edges[2].head_vertex, 2);
+  EXPECT_EQ(g.edges[3].vertex_set, 0b11100);
+  EXPECT_EQ(g.edges[3].head_vertex, 2);
+
+  EXPECT_EQ(g.vertices[0].degree_undirected, 2);
+  EXPECT_EQ(g.vertices[0].degree_head, 0);
+  EXPECT_EQ(g.vertices[0].degree_tail, 0);
+
+  EXPECT_EQ(g.vertices[1].degree_undirected, 1);
+  EXPECT_EQ(g.vertices[1].degree_head, 0);
+  EXPECT_EQ(g.vertices[1].degree_tail, 1);
+
+  EXPECT_EQ(g.vertices[2].degree_undirected, 1);
+  EXPECT_EQ(g.vertices[2].degree_head, 2);
+  EXPECT_EQ(g.vertices[2].degree_tail, 0);
+
+  EXPECT_EQ(g.vertices[3].degree_undirected, 2);
+  EXPECT_EQ(g.vertices[3].degree_head, 0);
+  EXPECT_EQ(g.vertices[3].degree_tail, 2);
+
+  EXPECT_EQ(g.vertices[4].degree_undirected, 0);
+  EXPECT_EQ(g.vertices[4].degree_head, 0);
+  EXPECT_EQ(g.vertices[4].degree_tail, 1);
 }
